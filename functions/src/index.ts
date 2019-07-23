562adfc28964492839b53c67b49326d0016adb10
dmayerdesign <d.a.mayer92@gmail.com>
2019-07-22 23:17:45 -0400

    Configure firestore and functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
-import * as functions from 'firebase-functions'
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+import * as functions from 'firebase-functions'
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!')
 })
+
+export const exampleOnWrite = functions.firestore
+  .document('sandbox/users/danny').onWrite((change, context) => {
+    console.log('CHANGE ===>', change, context)
+  })
