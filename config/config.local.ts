22af392102f10c4088d49f50b300736243cb0778
dmayerdesign <d.a.mayer92@gmail.com>
2019-09-22 01:23:24 -0400

    Get jest working

--- config/config.local.ts
+++ config/config.local.ts
@@ -1,2 +1,4 @@
-export const GCP_REGION = 'us-east1'
-export const FIRE_PROJECT_ID = 'funk-development'
+export {
+  GCP_REGION,
+  FIRE_PROJECT_ID
+} from './config.development'
--- functions/src/api/get-products.ts
+++ functions/src/api/get-products.ts
@@ -7,6 +7,5 @@ export default functions.https.onRequest(async (_request, response) => {
   const firestore = new Firestore({ projectId: functions.config().public.fire_project_id })
   const products = await firestore.collection('sandbox-users').get()
     .then(({ docs }) => docs) as Product[]
-  console.log(products)
-  return response.json(process.env.FIREBASE_CONFIG)
+  return response.json(products)
 })
