4775c3b9f74b4112e59f76e2d78a115b09ea624a
dmayerdesign <d.a.mayer92@gmail.com>
2020-06-14 16:21:27 -0400

    fix(functions): fix broken "order/get-tax" import

--- functions/src/app/commerce/order/get-tax.ts
+++ functions/src/app/commerce/order/get-tax.ts
@@ -1,7 +1,7 @@
 import createRpcFunction from "@funk/functions/helpers/http/create-rpc-function"
 import mapRequestToBody from "@funk/functions/helpers/http/map-request-to-body"
-import { construct as constructGetTax } from "@funk/model/commerce/order/actions/get-tax"
+import getTax from "@funk/api/commerce/order/get-tax"
 
 export default createRpcFunction(
-  mapRequestToBody(constructGetTax(getProductForSku))
+  mapRequestToBody(getTax)
 )
