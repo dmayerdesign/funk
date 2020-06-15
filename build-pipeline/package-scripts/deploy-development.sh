23824b9a23727cb1afdd5aa530025808313d9e1e
dmayerdesign <d.a.mayer92@gmail.com>
2020-06-15 00:06:59 -0400

    fix(*): rename test scripts

--- build-pipeline/package-scripts/deploy-development.sh
+++ build-pipeline/package-scripts/deploy-development.sh
@@ -1,4 +1,5 @@
 npm i && \
+npm run test && \
 npm run build::development && \
 firebase use funk-development && \
 firebase deploy
--- build-pipeline/package-scripts/test.sh
+++ build-pipeline/package-scripts/test.sh
@@ -1,2 +1,2 @@
-sh build-pipeline/package-scripts/test-browser.sh && \
-sh build-pipeline/package-scripts/test-server.sh
+sh build-pipeline/package-scripts/test-ui.sh && \
+sh build-pipeline/package-scripts/test-api.sh
