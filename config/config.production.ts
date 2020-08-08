33fa44190be0e243c84e3d9d12368a272f9cf509
dmayerdesign <d.a.mayer92@gmail.com>
2020-08-07 21:15:47 -0400

    chore: add "web.app" domain to trusted origins

--- config/config.production.ts
+++ config/config.production.ts
@@ -5,7 +5,7 @@ export const CLOUD_PROJECT_ID = "funk-development"
 export const CLOUD_PROJECT_REGION = "us-east1"
 export const FUNCTIONS_REGION = "us-central1"
 export const FUNCTIONS_BASE_URL = `https://${FUNCTIONS_REGION}-${CLOUD_PROJECT_ID}.cloudfunctions.net`
-export const TRUSTED_ORIGINS = "http://localhost:8100,https://funk-development.firebaseapp.com"
+export const TRUSTED_ORIGINS = "http://localhost:8100,https://funk-development.firebaseapp.com,https://funk-development.web.app"
 // TODO: Remove this in favor of setting the GOOGLE_APPLICATION_CREDENTIALS env variable.
 export const PATH_TO_SERVICE_ACCOUNT_JSON = "/Users/danielmayer/.secrets/funk-development/funk-development-093bbbc44257.json"
 export const TAX_RATE_CALCULATOR_URL = "https://rest.avatax.com/api/v2/taxrates/bypostalcode"
