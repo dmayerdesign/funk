41d851d8f437c402a3b9ea73a9234c4d8e124ad6
dmayerdesign <d.a.mayer92@gmail.com>
2020-09-16 23:25:34 -0400

    chore: run "npm audit fix"

--- build-pipeline/package-scripts/ui-build-development.sh
+++ build-pipeline/package-scripts/ui-build-development.sh
@@ -1,6 +1,6 @@
 sh build-pipeline/package-scripts/ts-node.sh ./build-pipeline/package-scripts/prebuild.ts -c "development" && \
 sh build-pipeline/package-scripts/ts-node.sh ./build-pipeline/package-scripts/ui-prebuild.ts -c "development" && \
 ionic build -c "development" && \
-ng run client-app:server -c "development"
-ionic capacitor copy ios --no-build
+ng run client-app:server -c "development" && \
+ionic capacitor copy ios --no-build && \
 ionic capacitor copy android --no-build
--- build-pipeline/package-scripts/ui-build-local.sh
+++ build-pipeline/package-scripts/ui-build-local.sh
@@ -1,6 +1,6 @@
 sh build-pipeline/package-scripts/ts-node.sh ./build-pipeline/package-scripts/prebuild.ts -c "local" && \
 sh build-pipeline/package-scripts/ts-node.sh ./build-pipeline/package-scripts/ui-prebuild.ts -c "local" && \
 ionic build -c "local" && \
-ng run client-app:server -c "local"
-ionic capacitor copy ios --no-build
+ng run client-app:server -c "local" && \
+ionic capacitor copy ios --no-build && \
 ionic capacitor copy android --no-build
