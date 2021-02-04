ac35166d4956ff581fcf174426936129707c20af
dmayerdesign <d.a.mayer92@gmail.com>
2021-02-03 21:45:52 -0500

    chore: add "external" dir to app-shell/infrastructure

--- ui/ssr/infrastructure/external/server.module.ts
+++ ui/ssr/infrastructure/external/server.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core"
 import { NoopAnimationsModule } from "@angular/platform-browser/animations"
 import { ServerModule } from "@angular/platform-server"
 import { RouterModule, Routes } from "@angular/router"
-import { AppShellComponent } from "@funk/ui/app-shell/infrastructure/component"
+import { AppShellComponent } from "@funk/ui/app-shell/infrastructure/external/component"
 import { AppModule } from "@funk/ui/infrastructure/external/app.module"
 import { AppComponent } from "@funk/ui/infrastructure/external/component"
 
